550eede0765e892df4f985deea38cf899d091d35
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-13 09:37:10 +0300

    completed

--- stores/admin.py
+++ stores/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Store
 
 # Register your models here.
-admin.site.register(Store)
+
+class StoreModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model=Store
+    list_display=['id','name','description']
+    search_fields=['name','description']
+
+admin.site.register(Store,StoreModelAdmin)
